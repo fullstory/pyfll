60db1a068ea833564b256882045210ef44cc8e72
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2008-07-20 12:01:48 +0000

    generate kde4 pinning automatically from Debian's source package svn.

---
+++ contrib/get-kde4-pinning.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+BASE_URL="svn://svn.debian.org/svn/pkg-kde/branches/kde4/packages"
+#SOURCE_PACKAGES="$(svn ls ${BASE_URL})"
+SOURCE_PACKAGES="
+	kdeadmin
+	kdeartwork
+	kdebase
+	kdebase-workspace
+	kdeedu
+	kdegames
+	kdegraphics
+	kdemultimedia
+	kdenetwork
+	kdepim
+	kdeutils
+	meta-kde4
+	systemsettings
+"
+unset BINARY_PACKAGES
+
+for i in $SOURCE_PACKAGES; do
+	BINARY_PACKAGES="$BINARY_PACKAGES $(svn cat "${BASE_URL}/${i}/debian/control" | awk '/^Package\:/{ print $2 }')"
+done
+
+printf "Explanation: this file provides apt-pinning\n"
+printf "Explanation: enabling the install of the debian\n"
+printf "Explanation: kde4-minimal meta-package in pyfll.\n"
+printf "Explanation: Add this files path to apt_preferences in fll.conf\n"
+
+printf "Package: *\nPin: release a=unstable\nPin-Priority: 500\n\n"
+printf "Package: *\nPin: release a=testing\nPin-Priority: 200\n\n"
+printf "Package: *\nPin: release a=experimental\nPin-Priority: 1\n"
+
+for i in $BINARY_PACKAGES; do
+	printf "\nPackage: ${i}\nPin: release a=experimental\nPin-Priority: 500\n"
+done
