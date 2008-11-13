34b9cb061cd9d8cae439b6c89b778e93035da0e9
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2008-11-13 20:08:41 +0000

    restructure kde4 preferences.

--- contrib/get-kde4-pinning.sh
+++ contrib/get-kde4-pinning.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 BASE_URL="svn://svn.debian.org/svn/pkg-kde/branches/kde4/packages"
-SOURCE_PACKAGES="$(svn ls ${BASE_URL})"
-unset BINARY_PACKAGES
-
-for i in $SOURCE_PACKAGES; do
-	BINARY_PACKAGES="$BINARY_PACKAGES $(svn cat "${BASE_URL}/${i}/debian/control" | awk '/^Package\:/{ print $2 }')"
-done
+SOURCE_PACKAGES="$(svn ls ${BASE_URL} | grep -v -e pkg-kde-tools | sed s/\\/$//)"
 
 printf "Explanation: this file provides apt-pinning\n"
 printf "Explanation: enabling the install of the debian\n"
@@ -15,8 +10,10 @@ printf "Explanation: Add this files path to apt_preferences in fll.conf\n"
 
 printf "Package: *\nPin: release a=unstable\nPin-Priority: 500\n\n"
 printf "Package: *\nPin: release a=testing\nPin-Priority: 200\n\n"
-printf "Package: *\nPin: release a=experimental\nPin-Priority: 1\n"
+printf "Package: *\nPin: release a=experimental\nPin-Priority: 101\n"
 
-for i in $BINARY_PACKAGES; do
-	printf "\nPackage: ${i}\nPin: release a=experimental\nPin-Priority: 500\n"
+for i in $SOURCE_PACKAGES; do
+	printf "\nExplanation: ${i}\nPackage: $(svn cat "${BASE_URL}/${i}/debian/control" | awk '/^Package\:/{ print $2 }' | xargs)\nPin: release a=experimental\nPin-Priority: 500\n"
 done
+
+printf "\n"
