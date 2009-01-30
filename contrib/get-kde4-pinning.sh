6aebcb49a0a4d6a05757454ad7a6952a3b0dffcf
Michael Deelwater <michael.deelwater@googlemail.com>
2009-01-30 22:30:47 +0000

    * contrib/get-kde4-pinning.sh:   - Use KDE 4.2 SVN branch.   - Add whitelist entries for pinning.   - Remove excluded source packages. * contrib/preferences:   - Newly generated by gen-kde4-pinning.sh.

--- contrib/get-kde4-pinning.sh
+++ contrib/get-kde4-pinning.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-BASE_URL="svn://svn.debian.org/svn/pkg-kde/branches/kde4/packages"
-SOURCE_PACKAGES="$(svn ls ${BASE_URL} | grep -v -e pkg-kde-tools -e automoc -e blitz -e soprano | sed s/\\/$//)"
+BASE_URL="svn://svn.debian.org/svn/pkg-kde/branches/kde4.2/packages"
+SOURCE_PACKAGES="$(svn ls ${BASE_URL} | grep -v -e pkg-kde-tools -e blitz | sed s/\\/$//)"
+
+# Additional packages needed from experimental
+WHITELIST="libstreamanalyzer0 kpilot superkaramba" 
 
 printf "Explanation: this file provides apt-pinning\n"
 printf "Explanation: enabling the install of the debian\n"
@@ -13,7 +16,14 @@ printf "Package: *\nPin: release a=testing\nPin-Priority: 200\n\n"
 printf "Package: *\nPin: release a=experimental\nPin-Priority: 101\n"
 
 for i in $SOURCE_PACKAGES; do
-	printf "\nExplanation: ${i}\nPackage: $(svn cat "${BASE_URL}/${i}/debian/control" | awk '/^Package\:/{ print $2 }' | grep -v -e ^libakonadiprivate1$ -e ^phonon$ -e ^libphonon-dev$ -e ^libphonon4$ | xargs)\nPin: release a=experimental\nPin-Priority: 500\n"
+	printf "\nExplanation: ${i}\nPackage: $(svn cat "${BASE_URL}/${i}/debian/control" | awk '/^Package\:/{ print $2 }' | xargs)\nPin: release a=experimental\nPin-Priority: 500\n"
 done
+
+# Whitelist entries
+printf "\nExplanation: Additional packages\nPackage: "
+for i in $WHITELIST; do
+	printf "$i "
+done
+printf "\nPin: release a=experimental\nPin-Priority: 500\n\n"
 
 printf "\n"
