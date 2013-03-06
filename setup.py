3ab703a2b4fcb10e931b5278de3838f16fc4dca5
Kel Modderman <kel@otaku42.de>
2013-03-06 04:33:13 +0000

    Add setup script and configuration for python package build

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+from glob import glob
+import os
+
+fll_prog = ['pyfll', 'fll.conf']
+fll_data = glob('data/*')
+fll_pkgs = [f for f in glob('packages/*') if os.path.isfile(f)]
+fll_pkgs_d = glob('packages/packages.d/*')
+
+setup(
+    name='pyfll',
+    author='Kelvin Modderman',
+    author_email='kel@otaku42.de',
+    license='GPL-2',
+    description='FULLSTORY live linux media mastering utility',
+    url='http://developer.berlios.de/projects/fullstory/',
+    scripts=['fll'],
+    data_files=[
+        ('/usr/share/fll/', fll_prog),
+        ('/usr/share/fll/data', fll_data),
+        ('/usr/share/fll/packages', fll_pkgs),
+        ('/usr/share/fll/packages/packages.d/', fll_pkgs_d),
+    ],
+)
